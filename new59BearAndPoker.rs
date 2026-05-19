f2e83c0dbdf66c279563c6af08ef3409a96b6588
Not-Buddy <kingeaary@gmail.com>
2026-05-19 15:53:08 +0000

    reverse digits

---
+++ new59BearAndPoker.rs
@@ -0,0 +1,38 @@
+use std::io;
+
+fn main(){
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).unwrap();
+    let _n : i32 = input.split_whitespace().next().unwrap().parse().unwrap(); 
+    input.clear();
+
+    io::stdin().read_line(&mut input).unwrap();
+    let vec : Vec<i32> = input.split_whitespace()
+    .map(|s| s.parse().unwrap())
+    .collect();
+
+    let maximum = *vec.iter().max().unwrap();
+
+    let mut possible = true;
+
+    for i in &vec {
+        if *i != maximum{
+            if maximum % i == 0{
+                continue;
+            }
+            else {
+                possible = false;
+                break;
+            }
+        }
+    }
+
+    if possible {
+        println!("Yes");
+    }
+    else {
+        println!("No");
+    }
+
+
+}
