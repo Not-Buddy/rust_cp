001abd16c0502c18fdbe3043f883d6cdd35da0a1
Not-Buddy <kingeaary@gmail.com>
2026-05-23 07:09:56 +0000

    fixed greedy apporach instead of simulation now counting

--- new65PolycarpTraining.rs
+++ new65PolycarpTraining.rs
@@ -5,7 +5,7 @@ fn main(){
     io::stdin().read_line(&mut input).expect("Failed to read line");
 
     let mut it = input.split_whitespace();
-    let n : usize  = it.next().unwrap().parse().unwrap();
+    let _n : usize  = it.next().unwrap().parse().unwrap();
     input.clear();
 
     io::stdin().read_line(&mut input).expect("Failed to read line");
@@ -14,13 +14,11 @@ fn main(){
     .collect();
 
     vec.sort();
-    
+
     let mut days_solved = 0;
 
-    for i in 0..n{
-        let target = i + 1;
-        if let Some(index) = vec.iter().position(|&x| x >= target){
-            vec.remove(index);
+    for val in vec{
+        if val >= days_solved + 1{
             days_solved += 1;
         }
     }
