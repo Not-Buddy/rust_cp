39dc0667ad616b8d064fc64fa6aeba294c148410
Not-Buddy <kingeaary@gmail.com>
2026-05-23 07:06:57 +0000

    1k greedy

---
+++ new65PolycarpTraining.rs
@@ -0,0 +1,32 @@
+use std::io;
+
+fn main(){
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("Failed to read line");
+
+    let mut it = input.split_whitespace();
+    let n : usize  = it.next().unwrap().parse().unwrap();
+    input.clear();
+
+    io::stdin().read_line(&mut input).expect("Failed to read line");
+    let mut vec : Vec<usize> = input.split_whitespace()
+    .map(|s| s.parse().unwrap())
+    .collect();
+
+    vec.sort();
+    
+    let mut days_solved = 0;
+
+    for i in 0..n{
+        let target = i + 1;
+        if let Some(index) = vec.iter().position(|&x| x >= target){
+            vec.remove(index);
+            days_solved += 1;
+        }
+    }
+
+    println!("{}", days_solved);
+
+    
+
+}
